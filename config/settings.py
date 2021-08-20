c6bc853709ed51a5005893608c8b38aaa109a042
cgvalezuelac <elgael06@gmail.com>
2021-08-20 09:58:04 -0600

    dircctorioss no necesarios.

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 TPL_DIR = os.path.join(BASE_DIR, 'build/')
-DB_FILE = os.path.join(BASE_DIR, 'database/database.sqlite3')
 CACHE_DIR = os.path.join(BASE_DIR, 'files/cache/')
 MEDIA = os.path.join(BASE_DIR, 'files/media/')
 STATIC = os.path.join(BASE_DIR, 'build/static/')

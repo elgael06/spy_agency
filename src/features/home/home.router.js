a0b63e2c763f2fa062a0131601adb756e58cad3e
cgvalezuelac <elgael06@gmail.com>
2021-08-22 23:23:18 -0600

    switch.

--- src/features/home/home.router.js
+++ src/features/home/home.router.js
@@ -10,14 +10,14 @@ const Home = lazy(()=>import("./home"));
 const Page404 = lazy(()=>import("./page-404"));
 
 const HomeRouter = () => (
-    <Switch>
-        <Route path='/login' component={()=><Redirect to='/home'  />} />
-        <Route path='/' exact component={()=><Redirect to='/home'  />} />
-        <Suspense fallback={<div>loading...</div>} >
+    <Suspense fallback={<div>loading...</div>} >
+        <Switch>
+            <Route path='/login' component={()=><Redirect to='/home'  />} />
+            <Route path='/' exact component={()=><Redirect to='/home'  />} />
             <Route path='/home' exact component={Home} />
-            <Route path='/*' component={Page404} />
-        </Suspense>
-    </Switch>
+            <Route path='/' component={Page404} />
+        </Switch>
+    </Suspense>
 );
 
 export  default  HomeRouter;
